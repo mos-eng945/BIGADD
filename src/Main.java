d5c75b2fc6a47c9c95bec7c68da10781810dd85b
mos <sbsjsfmdnfkgo@gmail.com>
2026-02-03 17:53:52 +0800

    大数组并行求和master2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("大数组并行求和程序启动");
         System.out.println("-----------------------------------");
-
         // 生成测试数据
         int size = 500000000;  // 5亿
         ArrayGenerator a =new ArrayGenerator();
